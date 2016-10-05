c81ef27f4310d644aaea7183bed1a833449f90d7
scarrillo9 <scarrillo7@miners.utep.edu>
2016-10-05 14:25:25 -0600

    Add files via upload

---
+++ Part1/User.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class User {
+	private String name;
+	private String username;
+	private String password;
+	
+	public void browseItem(){
+		
+	}//end browse item
+	
+	public void register(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter name:");
+		this.name = input.next();
+		System.out.println("Enter username:");
+		this.username = input.next();
+		System.out.println("Enter password:");
+		this.password = input.next();
+	}//end register
+	
+	
+	//GETTER METHODS//
+	public String getName(){
+		return name;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+
+	public String getPassword(){
+		return password;
+	}
+	
+	//SETTER METHODS//
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+}
